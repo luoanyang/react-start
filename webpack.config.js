41a671d26adefee85a99ef7e26686e21493e7eff
luoanyang <anyang.luo@flipos.cn>
2019-09-04 11:24:38 +0800

    增加箭头函数编译

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,8 @@ module.exports = {
       use: {
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-transform-arrow-functions', '@babel/plugin-proposal-class-properties']
         }
       }
     }]
